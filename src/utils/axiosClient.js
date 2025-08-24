82944f99ae458f57f628b8df2aee488e58b931d9
Avinash Kumar Jha <avinashaj004@gmail.com>
2025-08-24 09:58:12 +0530

    commit frontend

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://vercel-backend-7-djd1.onrender.com',
+    baseURL: 'https://vercel-backend-8-bdwh.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
